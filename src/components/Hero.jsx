ec5c4431d3020e0f14eb4194fae8ca1bc48f7680
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2024-05-01 23:59:34 +0530

    chanhes for mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       {/* now we will add a motion div that is using framer motion */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
